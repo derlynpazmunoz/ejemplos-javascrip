2226e9701ac3a0b92d7df579293b803d3b09af9e
derlynpazmunoz <derlyn.paz@misena.edu.co>
2022-06-24 20:33:40 -0500

    mi primer commit

---
+++ ejemplo1.js
@@ -0,0 +1,17 @@
+class Libro{
+constructor(autor,nombre,numeropagina){
+    this.autor=autor
+    this.nombre=nombre
+    this.numeropagina=numeropagina
+
+}
+set nombrelibro(nombre1){
+
+}
+get mostrarlibro(){
+
+}
+
+}
+let libro1=new Libro("gabriel garcia marquez","cien años de soledad","350")
+console.log(libro1)
